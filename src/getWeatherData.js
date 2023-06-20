bd12436fcc269c2e59a589be73622786069f6b1f
Andrew T <andrewtgit@gmail.com>
2023-06-20 19:11:27 +1000

    Added more displayed information.

--- src/getWeatherData.js
+++ src/getWeatherData.js
@@ -5,16 +5,32 @@ export default function getWeatherData() {
 
   function publishData(data) {
     // Publish successful datga
-    ps.publish("weather-country", `${data.location.country}`);
-    ps.publish("weather-temperature", `${data.current.temp_c}°C`);
-    ps.publish("weather-location", data.location.name);
+    ps.publish("update-country", `${data.location.country}`);
+    ps.publish("update-temperature", `${data.current.temp_c}°C`);
+    ps.publish("update-location", data.location.name);
+    ps.publish("update-wind-speed", `${data.current.wind_kph} km/h`);
+    ps.publish("update-wind-direction", data.current.wind_dir);
+    ps.publish("update-humidity", `${data.current.humidity}%`);
+    ps.publish("update-precipitation", `${data.current.precip_mm}mm`);
+    ps.publish("update-condition", data.current.condition.text);
+    ps.publish("update-weather-icon", data.current.condition.icon);
+    ps.publish("update-local-time", timeConverter(data.location.localtime));
+    ps.publish("update-forecast", sortForecast(data.forecast.forecastday));
   }
 
   function publishError() {
     // Publish error message
-    ps.publish("weather-country", "INVALID LOCATION");
-    ps.publish("weather-location", "INVALID LOCATION");
-    ps.publish("weather-temperature", "ERROR");
+    ps.publish("update-country", "INVALID LOCATION");
+    ps.publish("update-location", "INVALID LOCATION");
+    ps.publish("update-temperature", "ERROR");
+    ps.publish("update-wind-speed", "ERROR");
+    ps.publish("update-wind-direction", "ERROR");
+    ps.publish("update-humidity", "ERROR");
+    ps.publish("update-precipitation", "ERROR");
+    ps.publish("update-condition", "ERROR");
+    ps.publish("update-weather-icon", "");
+    ps.publish("update-local-time", "ERROR");
+    ps.publish("update-forecast", "ERROR");
   }
 
   function handleError(err) {
@@ -27,15 +43,70 @@ export default function getWeatherData() {
     // Get weather data
     try {
       const response = await fetch(
-        `https://api.weatherapi.com/v1/current.json?key=${weatherKey}&q=${input}`,
+        `https://api.weatherapi.com/v1/forecast.json?key=${weatherKey}&q=${input}&days=7`,
         { mode: "cors" }
       );
       const weatherData = await response.json();
-      console.log(weatherData);
       publishData(weatherData);
     } catch (err) {
       handleError(err);
     }
+  }
+
+  function sortForecast(forecastData) {
+    // Sort day, weather icon, high and low temps, hourly highs
+    const array = [];
+
+    for (let i = 0; i < forecastData.length; i += 1) {
+      const dayData = forecastData[i];
+      const obj = {
+        mintemp: dayData.day.mintemp_c,
+        maxtemp: dayData.day.maxtemp_c,
+        icon: dayData.day.condition.icon,
+        day: new Date(dayData.date).toLocaleDateString("en-US", {
+          weekday: "short",
+        }),
+      };
+      array.push(obj);
+    }
+
+    return array;
+  }
+
+  function timeConverter(timeData) {
+    let day = "";
+    // Switch case for day of week
+    switch (new Date(timeData).getDay()) {
+      case 0:
+        day = "Sunday";
+        break;
+      case 1:
+        day = "Monday";
+        break;
+      case 2:
+        day = "Tuesday";
+        break;
+      case 3:
+        day = "Wednesday";
+        break;
+      case 4:
+        day = "Thursday";
+        break;
+      case 5:
+        day = "Friday";
+        break;
+      case 6:
+        day = "Saturday";
+        break;
+    }
+
+    let time = new Date(timeData).toLocaleString("en-US", {
+      hour: "numeric",
+      minute: "numeric",
+      hour12: true,
+    });
+
+    return `${day} ${time}`;
   }
 
   ps.subscribe("get-weather", getWeather);
--- src/layout.js
+++ src/layout.js
@@ -4,34 +4,109 @@ export default function weatherPageLayout() {
   // Cache page elements
   const getWeatherBtn = document.getElementById("get-weather-btn");
   const input = document.querySelector("input");
-  const weatherCountry = document.getElementById("weather-country");
-  const weatherCity = document.getElementById("weather-city");
-  const weatherTemperature = document.getElementById("weather-output");
+  const country = document.getElementById("country");
+  const city = document.getElementById("city");
+  const temperature = document.getElementById("temperature");
+  const windSpeed = document.getElementById("wind-speed");
+  const windDirection = document.getElementById("wind-direction");
+  const humidity = document.getElementById("humidity");
+  const precipitation = document.getElementById("precipitation");
+  const condition = document.getElementById("condition");
+  const weatherIcon = document.getElementById("weather-icon");
+  const localTime = document.getElementById("local-time");
+  const forecast = document.getElementById("forecast");
 
-  function updateCountry(country) {
-    weatherCountry.textContent = `Country: ${country}`;
+  function updateCountry(countryData) {
+    country.textContent = `Country: ${countryData}`;
   }
 
-  function updateCity(location) {
+  function updateCity(locationData) {
     // Update location
-    weatherCity.textContent = `City: ${location}`;
+    city.textContent = `City: ${locationData}`;
   }
 
-  function updateTemperature(temperature) {
+  function updateTemperature(temperatureData) {
     // Update temperature
-    weatherTemperature.textContent = `Temperature: ${temperature}`;
+    temperature.textContent = `Temperature: ${temperatureData}`;
+  }
+
+  function updateWindSpeed(windSpeedData) {
+    // Update wind speed
+    windSpeed.textContent = `Wind Speed: ${windSpeedData}`;
+  }
+
+  function updateWindDirection(windDirectionData) {
+    // Update wind direction
+    windDirection.textContent = `Wind direction: ${windDirectionData}`;
+  }
+
+  function updateHumidity(humidityData) {
+    // Update humidity
+    humidity.textContent = `Humidity: ${humidityData}`;
+  }
+
+  function updatePrecipitation(precipitationData) {
+    // Update precipitation
+    precipitation.textContent = `Precipitation: ${precipitationData}`;
+  }
+
+  function updateCondition(conditionData) {
+    // Update condition
+    condition.textContent = `Condition: ${conditionData}`;
+  }
+
+  function updateWeatherIcon(weatherIconData) {
+    // Update weather icon
+    weatherIcon.src = weatherIconData;
+  }
+
+  function updateLocalTime(localTimeData) {
+    // Update local time
+    localTime.textContent = `Local time: ${localTimeData}`;
+  }
+
+  function updateForecast(foreCastData) {
+    // Update 7 day forecast
+    const liElems = forecast.getElementsByTagName("li");
+
+    foreCastData.forEach((data, index) => {
+      // Get each list item by index then update each element
+      const dayElem = liElems[index].querySelector(".forecast-day");
+      const iconElem = liElems[index].querySelector(".forecast-icon");
+      const maxElem = liElems[index].querySelector(".forecast-max");
+      const minElem = liElems[index].querySelector(".forecast-min");
+
+      dayElem.textContent = data.day;
+      iconElem.src = data.icon;
+      maxElem.textContent = `${data.maxtemp}°C`;
+      minElem.textContent = `${data.mintemp}°C`;
+    });
   }
 
   function getWeather() {
-    let city = input.value;
-    weatherCountry.textContent = "Country: Loading...";
-    weatherCity.textContent = "City: Loading...";
-    weatherTemperature.textContent = "Current temperature: Loading...";
-    ps.publish("get-weather", city);
+    let request = input.value;
+    country.textContent = "Country: Loading...";
+    city.textContent = "City: Loading...";
+    temperature.textContent = "Current temperature: Loading...";
+    windSpeed.textContent = "Wind speed: Loading...";
+    windDirection.textContent = "Wind direction: Loading...";
+    humidity.textContent = "Humidity: Loading...";
+    precipitation.textContent = "Precipitation: Loading...";
+    condition.textContent = "Condition: Loading...";
+    localTime.textContent = "Local time: Loading...";
+    setTimeout(ps.publish("get-weather", request), 5000);
   }
 
   getWeatherBtn.addEventListener("click", getWeather);
-  ps.subscribe("weather-country", updateCountry);
-  ps.subscribe("weather-location", updateCity);
-  ps.subscribe("weather-temperature", updateTemperature);
+  ps.subscribe("update-country", updateCountry);
+  ps.subscribe("update-location", updateCity);
+  ps.subscribe("update-temperature", updateTemperature);
+  ps.subscribe("update-wind-speed", updateWindSpeed);
+  ps.subscribe("update-wind-direction", updateWindDirection);
+  ps.subscribe("update-humidity", updateHumidity);
+  ps.subscribe("update-precipitation", updatePrecipitation);
+  ps.subscribe("update-condition", updateCondition);
+  ps.subscribe("update-weather-icon", updateWeatherIcon);
+  ps.subscribe("update-local-time", updateLocalTime);
+  ps.subscribe("update-forecast", updateForecast);
 }
